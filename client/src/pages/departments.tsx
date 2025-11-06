dffdc8efc8e360a8aeb993b5334ff192fb3d86fd
AL-SILMI <mohamed.al.silmi05@gmail.com>
2025-11-06 19:34:23 +0530

    Fix import case for Navbar component

--- client/src/pages/departments.tsx
+++ client/src/pages/departments.tsx
@@ -5,7 +5,7 @@ import { useState } from "react";
 import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
-import Navbar from "@/components/navbar";
+import Navbar from "@/components/Navbar";
 import { departments } from "@/data/mockData";
 import { cn } from "@/lib/utils";
 
